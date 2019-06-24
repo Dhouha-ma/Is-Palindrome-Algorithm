d5568b9ff791e33fa8aaeda97b8b800cf48b72ba
Dhouha Mansour <dhouha.mansour11@gmail.com>
2019-06-24 22:25:20 +0200

    Update ispalindrome.js

--- ispalindrome.js
+++ ispalindrome.js
@@ -1,12 +1,3 @@
-/* 
-  
-Is Palindrome Algorithm 
-  
-The function will take a string as an argument,return true if the string is a palindrome and false if the string is not a palindrome. 
-Palindrome: a phrase that is spelled the same way both backward and forward. 
-  
-*/ 
-  
 function isPalindrome (text) { 
         text = text.toLowerCase(); 
         var textArr = text.split(''); 
